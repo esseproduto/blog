257666757d154f13560c0db557cf625b10220f97
Ellison Leão <ellisonleao@gmail.com>
2015-12-18 00:17:02 -0200

    pelican master branch

---
+++ README.md
@@ -0,0 +1,19 @@
+EsseProduto Blog
+================
+
+1 - Python 3.5
+2 - mkvirtualenv esseblog
+3 - pip install -r requirements.txt
+
+
+# Creating post
+
+	make newpost="TITLE"
+
+# Build html
+
+	make html
+
+# Running locally
+
+	make serve [PORT=7000]
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'EsseProduto'
+SITENAME = 'Blog EsseProduto'
+SITEURL = 'http://localhost:7000'
+
+PATH = 'content'
+STATIC_PATHS = ['blog']
+ARTICLE_PATHS = ['blog']
+ARTICLE_SAVE_AS = '{slug}.html'
+ARTICLE_URL = '{slug}'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# THEME SPECIFIC
+DESCRIPTION = 'Confiabilidade através de prova social na aquisição de produtos digitais'
+THEME = 'pelican-semantic-ui/'
+TWITTER_USERNAME = 'esseproduto'
